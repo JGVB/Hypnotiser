bacdabcf528904ab4ca935f7a0fb73834f1f3ce3
James VanBeverhoudt <jvanbev1@gmail.com>
2014-05-08 14:25:00 -0400

    Initial Commit

---
+++ Hypnotiser/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Hypnotiser
+//
+//  Created by James VanBeverhoudt on 5/8/14.
+//  Copyright (c) 2014 noOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ HypnotiserTests/HypnotiserTests.m
@@ -0,0 +1,34 @@
+//
+//  HypnotiserTests.m
+//  HypnotiserTests
+//
+//  Created by James VanBeverhoudt on 5/8/14.
+//  Copyright (c) 2014 noOrg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HypnotiserTests : XCTestCase
+
+@end
+
+@implementation HypnotiserTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
