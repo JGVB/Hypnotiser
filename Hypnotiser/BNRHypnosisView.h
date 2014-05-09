977feb30834ebd596116b16614e1378164bbab5c
James VanBeverhoudt <jvanbev1@gmail.com>
2014-05-08 22:56:21 -0400

    Circle Functionality

--- Hypnotiser/BNRHypnosisView.h
+++ Hypnotiser/BNRHypnosisView.h
@@ -1,16 +1,13 @@
 //
-//  AppDelegate.h
+//  BNRHypnosisView.h
 //  Hypnotiser
 //
 //  Created by James VanBeverhoudt on 5/8/14.
 //  Copyright (c) 2014 noOrg. All rights reserved.
 //
-//Add to github
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property(nonatomic, strong)UIWindow *window;
+@interface BNRHypnosisView : UIView
 
 @end
---
+++ Hypnotiser/BNRHypnosisView.m
@@ -0,0 +1,65 @@
+//
+//  BNRHypnosisView.m
+//  Hypnotiser
+//
+//  Created by James VanBeverhoudt on 5/8/14.
+//  Copyright (c) 2014 noOrg. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        // All BNRHypnosisViews start with a clear background color
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+    //Get bounds first
+    CGRect bounds = self.bounds;
+    //Get center of bounds
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2;
+    center.y = bounds.origin.y + bounds.size.height / 2;
+    
+    // The largest circle will circumscribe the view
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
+
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    // Add an arc to the path at center, with radius of radius,
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        [path addArcWithCenter:center
+                        radius:currentRadius // Note this is currentRadius!
+                    startAngle:0.0
+                      endAngle:M_PI * 2.0
+                     clockwise:YES];
+    }
+
+    
+    // Configure line width to 10 points
+    path.lineWidth = 10;
+    
+    
+    // Configure the drawing color to light gray
+    [[UIColor lightGrayColor] setStroke];
+    
+    [path stroke];
+    
+}
+
+
+@end
